2274082a0a2ce5ca28ae114f1583a1f564a82542
fer-Desarrollo <fernandoGT.Desarrollo@hotmal.com>
2025-09-09 10:52:41 -0600

    segundo commit

--- ejemplo.js
+++ ejemplo.js
@@ -1,1 +1,6 @@
-console.log("Hola Mundo");
+
+        // FUNCIÓN INCOMPLETA: tu compañero debe completarla
+        function sumarNumeros() {
+            // TODO: Obtener los valores de los inputs, sumarlos y mostrar el resultado en el párrafo con id "resultado"
+        }
+
